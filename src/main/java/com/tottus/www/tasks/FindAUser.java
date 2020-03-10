554bc4f34e8eeb86cd32e704d5868462eb2797c3
Juan Francisco Builes <pacho0323@hotmail.com>
2020-03-10 14:14:08 -0500

    Corregir interacción que estaba fallando

--- src/main/java/com/tottus/www/tasks/FindAUser.java
+++ src/main/java/com/tottus/www/tasks/FindAUser.java
@@ -1,33 +1,24 @@
 package com.tottus.www.tasks;
 
-import static com.tottus.www.utils.Constantes.RECURSO;
 import static net.serenitybdd.screenplay.Tasks.instrumented;
 
+import com.tottus.www.interactions.SendMessage;
 import com.tottus.www.models.User;
 
 import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Task;
-import net.serenitybdd.screenplay.rest.interactions.Get;
 
 public class FindAUser implements Task {
-	private String token;
-	private String firstName;
-	private String lastName;
+	private User user;
 
 	public FindAUser(User user) {
-		this.token = user.getToken();
-		this.firstName = user.getFirstName();
-		this.lastName = user.getLastName();
+		this.user = user;
 	}
 
 	@Override
 	public <T extends Actor> void performAs(T actor) {
 
-		actor.attemptsTo(Get.resource(RECURSO)
-				.with(request -> request
-				.param("access-token", token)
-				.param("first_name", firstName)
-				.param("last_name", lastName))				
+		actor.attemptsTo(SendMessage.get(user)		
 				);
 	}
 
