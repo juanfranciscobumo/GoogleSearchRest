0cce99982272f88163d9f394363f34bd0deb4f97
Juan Francisco Builes Montoya <juanbuiles00@users.noreply.github.com>
2024-02-07 11:55:39 -0500

    Juan builes
    Se actualiza la version de serenity-bdd

--- src/test/java/com/google/www/runners/GoogleSearchRunner.java
+++ src/test/java/com/google/www/runners/GoogleSearchRunner.java
@@ -1,17 +1,19 @@
 package com.google.www.runners;
 
 
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.cucumber.CucumberWithSerenity;
-import org.junit.runner.RunWith;
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.CucumberOptions.SnippetType.CAMELCASE;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
-@RunWith(CucumberWithSerenity.class)
-@CucumberOptions(
-        features = "src/test/resources/com/google/www/features/googleSearch.feature"
-        , glue = "com/google/www/stepdefinitions"
-        , snippets = CAMELCASE)
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("/features")
+@ConfigurationParameter(key= "cucumber.features", value="src/test/resources/com/google/www/features/googleSearch.feature")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
 public class GoogleSearchRunner {
 
 }
